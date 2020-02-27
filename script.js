3fb8ffafab18352adfd4c6a492d129e313b88b8f
Wood-sudo <61282204+Wood-sudo@users.noreply.github.com>
2020-02-27 15:57:39 +0000

    CSS

--- script.js
+++ script.js
@@ -34,18 +34,18 @@ function playerPaper () {
      compChoice = computerSelection();
 
      if(compChoice === playerChoice) {
-        return   tar('scoreBoard').innerHTML = `It's a tie! Both played ${playerChoice}`;
+        return   tar('gameOutcome').innerHTML = `It's a tie! Both played ${playerChoice}`;
      } else {
          if (playerChoice === 'Rock') {
              switch (compChoice) {
                  case 'Paper' :
                     compScore = tar('compScore').innerHTML = ++compScore;
-                    tar('scoreBoard').innerHTML = 'You Lose! Paper beats Rock.';
+                    tar('gameOutcome').innerHTML = 'You Lose! Paper beats Rock.';
                     scoreTracker ();
                     break;
                 default:
                     playerScore = tar('playerScore').innerHTML = ++playerScore;
-                    tar('scoreBoard').innerHTML = 'You Win! Rock beats Scissors.';
+                    tar('gameOutcome').innerHTML = 'You Win! Rock beats Scissors.';
                     scoreTracker ();
 
                      
@@ -54,12 +54,12 @@ function playerPaper () {
              switch (compChoice) {
                  case 'scissors':
                     compScore = tar('compScore').innerHTML = ++compScore;
-                    tar('scoreBoard').innerHTML = 'You Lose! Scissors beats Paper.';
+                    tar('gameOutcome').innerHTML = 'You Lose! Scissors beats Paper.';
                     scoreTracker ();
                     break;
                 default:
                     playerScore = tar('playerScore').innerHTML = ++playerScore;
-                    tar('scoreBoard').innerHTML = 'You Win! Paper beats Rock.';
+                    tar('gameOutcome').innerHTML = 'You Win! Paper beats Rock.';
                     scoreTracker ();
 
 
@@ -68,12 +68,12 @@ function playerPaper () {
              switch (compChoice) {
                  case 'Rock' :
                     compScore = tar('compScore').innerHTML = ++compScore;
-                    tar('scoreBoard').innerHTML = 'You Lose! Rock beats Scissors.';
+                    tar('gameOutcome').innerHTML = 'You Lose! Rock beats Scissors.';
                     scoreTracker ();
                     break;
                 default:
                     playerScore = tar('playerScore').innerHTML = ++playerScore;
-                    tar('scoreBoard').innerHTML = 'You Win! Scissors beats Paper.';
+                    tar('gameOutcome').innerHTML = 'You Win! Scissors beats Paper.';
                     scoreTracker ();
 
 
@@ -87,12 +87,12 @@ function playerPaper () {
     let playerSelection = 'Rock';    
     let compChoice = computerSelection(rps);
         if (compChoice === 'Rock') {             
-          return   tar('scoreBoard').innerHTML = 'It\'s a tie! Both play Rock!';            
+          return   tar('gameOutcome').innerHTML = 'It\'s a tie! Both play Rock!';            
         } else if (compChoice === 'scissors') {
-         return tar('scoreBoard').innerHTML = 'You Win! Rock beats Scissors.';
+         return tar('gameOutcome').innerHTML = 'You Win! Rock beats Scissors.';
             playerScore = tar('playerScore').innerHTML = ++playerScore;
         } else if (compChoice === 'Paper') {
-            tar('scoreBoard').innerHTML = 'You Lose! Paper beats Rock.';
+            tar('gameOutcome').innerHTML = 'You Lose! Paper beats Rock.';
             compScore = tar('compScore').innerHTML = ++compScore;
         }
     }; 
